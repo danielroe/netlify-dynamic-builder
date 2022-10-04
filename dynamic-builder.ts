579fb7f6589b42c81a0a5b267c74673a302f605b
Daniel Roe <daniel@roe.dev>
2022-10-04 09:58:23 +0100

    fix: override ttl

--- dynamic-builder.ts
+++ dynamic-builder.ts
@@ -1,31 +1,31 @@
-import '#internal/nitro/virtual/polyfill';
+import '#internal/nitro/virtual/polyfill'
 
-import { withQuery } from 'ufo';
-import { createRouter as createMatcher } from 'radix3';
+import { withQuery } from 'ufo'
+import { createRouter as createMatcher } from 'radix3'
 
-import { useRuntimeConfig } from '#internal/nitro';
-import { handler as _handler } from '#internal/nitro/entries/aws-lambda';
+import { useRuntimeConfig } from '#internal/nitro'
+import { handler as _handler } from '#internal/nitro/entries/aws-lambda'
 
-export const handler = async function handler(event, context) {
-  const config = useRuntimeConfig();
-  const routerOptions = createMatcher({ routes: config.nitro.routes });
+export const handler = async function handler (event, context) {
+  const config = useRuntimeConfig()
+  const routerOptions = createMatcher({ routes: config.nitro.routes })
 
   const query = {
     ...event.queryStringParameters,
     ...event.multiValueQueryStringParameters,
-  };
-  const url = withQuery(event.path || event.rawPath, query);
-  const routeOptions = routerOptions.lookup(url) || {};
+  }
+  const url = withQuery(event.path || event.rawPath, query)
+  const routeOptions = routerOptions.lookup(url) || {}
 
   if (routeOptions.static || routeOptions.swr) {
     const builder = await import('@netlify/functions').then(
       (r) => r.builder || r.default.builder
-    );
-    const ttl = typeof routeOptions.swr === 'number' ? routeOptions.swr : 60;
+    )
+    const ttl = typeof routeOptions.swr === 'number' ? routeOptions.swr : 60
     return Promise.resolve(builder(_handler)(event as any, context)).then((r) =>
-      routeOptions.swr ? { ttl, ...r } : r
-    );
+      routeOptions.swr ? { ...r, ttl } : r
+    )
   }
 
-  return _handler(event, context);
-};
+  return _handler(event, context)
+}
