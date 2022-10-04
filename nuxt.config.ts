01d39a2260551779f52d40aee0a3a34780befed3
Daniel Roe <daniel@roe.dev>
2022-10-04 09:30:00 +0100

    feat: add builder function rewrites

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+import { join } from 'pathe'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
@@ -5,13 +8,26 @@ export default defineNuxtConfig({
       '/articles': { swr: 360 },
       '/blog/**': { static: true },
     },
+    hooks: {
+      async 'compiled' (nitro) {
+        const redirectsPath = join(nitro.options.output.publicDir, '_redirects')
+        let contents = '/* /.netlify/functions/server 200'
+        if (existsSync(redirectsPath)) {
+          contents = readFileSync(redirectsPath, 'utf-8')
+        }
+        for (const [key] of Object.entries(nitro.options.routes).filter(([_, value]) => value.swr || value.static)) {
+          contents = `${key.replace('/**', '/*')}\t/.netlify/builders/server 200\n` + contents
+        }
+        writeFileSync(redirectsPath, contents)
+      },
+    }
   },
   hooks: {
-    'nitro:config'(nitroOptions) {
-      if (nitroOptions.dev) return;
+    'nitro:config' (nitroOptions) {
+      if (nitroOptions.dev) return
 
-      nitroOptions.entry = '~/dynamic-builder';
-      nitroOptions.preset = 'netlify';
+      nitroOptions.entry = '~/dynamic-builder'
+      nitroOptions.preset = 'netlify'
     },
   },
-});
+})
